009a563d5d103ec16abaa74f0355847260da6774
Sabin Bajracharya <sabin.bajracharya@gmail.com>
2021-06-10 23:09:17 +0545

    Add: Show list of endpoints in home page

--- dashboard/src/routes/index.svelte
+++ dashboard/src/routes/index.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let endpoints_promise = fetchEndpoints();
+
+    async function fetchEndpoints() {
+        const res = await fetch('http://localhost:8080/endpoints');
+        const json = await res.json();
+
+		if (res.ok) {
+			return json;
+		} else {
+			throw new Error(json);
+		}
+	}
+</script>
+
+<h1>Api Endpoints</h1>
+
+{#await endpoints_promise}
+	<p>...waiting</p>
+{:then items}
+    <ul>
+        {#each items as item (item.id)}
+            <li><a href="#">{item.path}</a></li>
+        {/each}
+    </ul>
+{:catch error}
+	<p style="color: red">{error.message}</p>
+{/await}
+
